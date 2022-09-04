76a507e94e46c000bced7db185b88ff8f846b0ac
SERKAN MUSTAFA <serkan2018@abv.bg>
2022-09-04 18:30:17 +0300

    RageExpenses

---
+++ src/Java1/RageExpenses.java
@@ -0,0 +1,37 @@
+package Java1;
+
+import java.util.Scanner;
+
+public class RageExpenses {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int countKeyboardTrashes = 0;
+        int lostGames = Integer.parseInt(scanner.nextLine());
+        double headsetPrice = Double.parseDouble(scanner.nextLine());
+        double mousePrice = Double.parseDouble(scanner.nextLine());
+        double keyboardPrice = Double.parseDouble(scanner.nextLine());
+        double displayPrice = Double.parseDouble(scanner.nextLine());
+
+        double expenses = 0;
+        for (int i = 1; i <= lostGames; i++) {
+            if (i % 2 == 0) {
+                expenses += headsetPrice;
+            }
+            if (i % 3 == 0) {
+                expenses += mousePrice;
+            }
+
+            if (i % 2 == 0 && i % 3 == 0) {
+                countKeyboardTrashes++;
+                expenses += keyboardPrice;
+
+                if (countKeyboardTrashes % 2 == 0) {
+                    expenses += displayPrice;
+                }
+            }
+
+
+        }
+        System.out.printf("Rage expenses: %.2f lv.", expenses);
+    }
+}
